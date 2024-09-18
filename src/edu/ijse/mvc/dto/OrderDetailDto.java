73e6486142cce569c8f94988d4e6ba6a1930b237
Anjana Thrishakya <anjanathrishakya@gmail.com>
2024-09-18 11:47:17 +0530

    OrderDto and OrderDetailDto

---
+++ src/edu/ijse/mvc/dto/OrderDetailDto.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.ijse.mvc.dto;
+
+/**
+ *
+ * @author anjan
+ */
+public class OrderDetailDto {
+    private String itemCode;
+    private int qty;
+    private double discount;
+
+    public OrderDetailDto() {
+    }
+
+    public OrderDetailDto(String itemCode, int qty, double discount) {
+        this.itemCode = itemCode;
+        this.qty = qty;
+        this.discount = discount;
+    }
+
+    /**
+     * @return the itemCode
+     */
+    public String getItemCode() {
+        return itemCode;
+    }
+
+    /**
+     * @param itemCode the itemCode to set
+     */
+    public void setItemCode(String itemCode) {
+        this.itemCode = itemCode;
+    }
+
+    /**
+     * @return the qty
+     */
+    public int getQty() {
+        return qty;
+    }
+
+    /**
+     * @param qty the qty to set
+     */
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    /**
+     * @return the discount
+     */
+    public double getDiscount() {
+        return discount;
+    }
+
+    /**
+     * @param discount the discount to set
+     */
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderDetailDto{" + "itemCode=" + itemCode + ", qty=" + qty + ", discount=" + discount + '}';
+    } 
+    
+}
---
+++ src/edu/ijse/mvc/dto/OrderDto.java
@@ -0,0 +1,94 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.ijse.mvc.dto;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author anjan
+ */
+public class OrderDto {
+    
+    private String orderId;
+    private String orderDate;
+    private String custId;
+    
+    private ArrayList<OrderDetailDto> orderDetailDtos;
+
+    public OrderDto() {
+    }
+
+    public OrderDto(String orderId, String orderDate, String custId, ArrayList<OrderDetailDto> orderDetailDtos) {
+        this.orderId = orderId;
+        this.orderDate = orderDate;
+        this.custId = custId;
+        this.orderDetailDtos = orderDetailDtos;
+    }
+
+    /**
+     * @return the orderId
+     */
+    public String getOrderId() {
+        return orderId;
+    }
+
+    /**
+     * @param orderId the orderId to set
+     */
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    /**
+     * @return the orderDate
+     */
+    public String getOrderDate() {
+        return orderDate;
+    }
+
+    /**
+     * @param orderDate the orderDate to set
+     */
+    public void setOrderDate(String orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    /**
+     * @return the custId
+     */
+    public String getCustId() {
+        return custId;
+    }
+
+    /**
+     * @param custId the custId to set
+     */
+    public void setCustId(String custId) {
+        this.custId = custId;
+    }
+
+    /**
+     * @return the orderDetailDtos
+     */
+    public ArrayList<OrderDetailDto> getOrderDetailDtos() {
+        return orderDetailDtos;
+    }
+
+    /**
+     * @param orderDetailDtos the orderDetailDtos to set
+     */
+    public void setOrderDetailDtos(ArrayList<OrderDetailDto> orderDetailDtos) {
+        this.orderDetailDtos = orderDetailDtos;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderDto{" + "orderId=" + orderId + ", orderDate=" + orderDate + ", custId=" + custId + ", orderDetailDtos=" + orderDetailDtos + '}';
+    }
+    
+    
+    
+}
